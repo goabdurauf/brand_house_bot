117463e84cd0dc94fe3b79fbb563687425bd582b
abdurauf <a.abdusattorov@axxessio.com>
2025-10-24 20:07:39 +0500

    initial code2

--- src/main/java/com/aarouf/telegrambot/TelegramBotApplication.java
+++ src/main/java/com/aarouf/telegrambot/TelegramBotApplication.java
@@ -27,53 +27,47 @@ public class TelegramBotApplication {
     }
 
     @MessageHandler(commands = "start")
-    private void start(BotContext ctx, Message message){
-        // Ask for the user's name
-        ctx.sendMessage(message.from.id, "Hey! What is your name?").exec();
+    void start(BotContext context, Message message){
 
-        // Set the user's state to "name" to indicate we're expecting their name next
-        ctx.setState(message.from.id, "name");
+        // Create a reply keyboard with resize option for better mobile appearance
+        var keyboard = new ReplyKeyboardMarkup().resizeKeyboard(true);
+
+        // Add buttons to the keyboard (in a single row by default)
+        keyboard.add("UZB", "RUS");
+
+        // Send welcome message with the keyboard attached
+        context.sendMessage(message.chat.id, "Welcome! Please choose an option:")
+                .replyMarkup(keyboard) // Attach the keyboard to the message
+                .exec();
     }
 
-    @MessageHandler(texts = {"ID", "Username", "Language", "test"})
+    @MessageHandler(texts = {"UZB", "RUS"})
     void text(BotContext context, Message message){
         var user = message.from;
         String response;
+        var keyboard = new ReplyKeyboardMarkup().resizeKeyboard(true);
 
         switch (message.text) {
-            case "ID":
-                response = String.format("<b>Your ID is:</b> <code>%d</code>", user.id);
+            case "UZB":
+                keyboard.add("LEPKA", "DIZAY");
                 break;
 
-            case "Username":
-                if (user.username == null) {
-                    response = "<i>You don't have a username set in your Telegram profile.</i>";
-                } else {
-                    response = String.format("<b>Your username is:</b> @%s", user.username);
-                }
+            case "RUS":
+                keyboard.add("LEPKA_RU", "DIZAY_RU");
                 break;
-
-            case "Language":
-                String language = (user.language_code != null) ? user.language_code : "not specified";
-                response = String.format("<b>Your language code is:</b> %s", language);
-                break;
-            case "test":
-                response = "hello world";
-                break;
-
             default:
                 response = "Unknown option selected.";
         }
 
-        context.sendMessage(message.chat.id, response)
-                .parseMode(ParseMode.HTML)
+        context.sendMessage(message.chat.id, "OPTIONLAR")
+                .replyMarkup(keyboard) // Attach the keyboard to the message
                 .exec();
     }
 
 
-    @MessageHandler(type = MessageType.TEXT, priority = 1)
-    void echo(BotContext context, Message message){
-        context.sendMessage(message.chat.id, "You said: " + message.text).exec();
-    }
+//    @MessageHandler(type = MessageType.TEXT, priority = 1)
+//    void echo(BotContext context, Message message){
+//        context.sendMessage(message.chat.id, "You said: " + message.text).exec();
+//    }
 
 }
